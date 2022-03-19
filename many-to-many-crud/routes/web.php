2ceee72006edca746ebb50fa3239b1e8ac5b5879
shakilkhan1414 <khan.shakil.1414@gmail.com>
2022-03-19 20:56:53 +0600

    many to many relations

--- many-to-many-crud/routes/web.php
+++ many-to-many-crud/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,53 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert',function(){
+    $user=User::findOrFail(1);
+    $role=new Role([
+        'name'=>'Admin'
+    ]);
+    $user->roles()->save($role);
+});
+
+Route::get('/read',function(){
+    $user=User::findOrFail(1);
+    foreach ($user->roles as $role) {
+        dd($role);
+    }
+});
+
+Route::get('/update',function(){
+    $user=User::findOrFail(1);
+    if($user->has('roles')){
+        foreach($user->roles as $role){
+            if($role->name=='Admin'){
+                $role->name='user';
+                $role->save();
+            }
+        }
+    }
+});
+
+Route::get('/delete',function(){
+    $user=User::findOrFail(1);
+    foreach($user->roles as $role){
+        $role->whereId(5)->delete();
+    }
+});
+
+Route::get('/attach',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->attach(3);
+});
+
+Route::get('/detach',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->detach(3);
+});
+
+Route::get('/sync',function(){
+    $user=User::findOrFail(1);
+    $user->roles()->sync([3,9]);
 });
