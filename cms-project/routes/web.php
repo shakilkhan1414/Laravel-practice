e480419d01a0274e1cd34e0ed7438540c82ca83d
shakilkhan1414 <khan.shakil.1414@gmail.com>
2022-03-30 06:37:12 +0600

    frontend admin-dashboard blog single-blog

---
+++ cms-project/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+
