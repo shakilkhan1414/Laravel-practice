cb5eed6722bc5c2c31e29e4c5083e8393987a5f7
shakilkhan1414 <khan.shakil.1414@gmail.com>
2023-06-12 15:03:45 +0600

    small fix

--- blogify/resources/views/layouts/app.blade.php
+++ blogify/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Blogry</title>
+  <title>Blogify</title>
 
   <link rel="shortcut icon" href="{{asset('img/favicon.png')}}" type="image/x-icon">
   <!-- Bootstrap core CSS -->
@@ -25,7 +25,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand logo-brand" href="/">Blogry</a>
+      <a class="navbar-brand logo-brand" href="/">Blogify</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
